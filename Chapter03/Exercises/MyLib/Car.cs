f7f3d34f1bc68a301e578e5e37955dd27e227df3
alekslast <alekslastt@gmail.com>
2023-12-12 20:40:20 +0200

    Add first half of the chapter. Features tested.

---
+++ Chapter03/Exercises/MyLib/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace MyLib;
+
+public class Car
+{
+    string name;
+    public Car(string name)
+    {
+        this.name = name;
+    }
+    public void GetInfo() => Console.WriteLine($"Car's name: {name}");
+}
